72fb8684c40ab78a9fd406b5c2b529dc3e81df3a
cahyaind <cahyaindsi@gmail.com>
2022-04-26 19:24:29 +0700

    belajar mengenai stateful widget, membuat user interface

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  void tekanTombol() {}
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Statfull Widget"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("7"),
+              RaisedButton(
+                child: Text("Tambah"),
+                onPressed: tekanTombol,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
